ea5a84b390ea280fbd0abcc402965424c6c1d740
borisbasic <basic2006@hotmail.com>
2023-03-06 18:47:36 +0100

    First commit!
    :wq
    
    :wq
    :q
    
    quit
    quit()
    :q
    :q
    :q!
    

---
+++ blocklist.py
@@ -0,0 +1,1 @@
+BLOCKLIST = set()  #for storing JWT is the best in the DATABASE or the best best is REDIS
---
+++ resources/item.py
@@ -0,0 +1,66 @@
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
+from flask_jwt_extended import jwt_required, get_jwt
+
+from db import db
+
+from models import ItemModel
+from schemas import ItemSchema, ItemUpdateSchema
+
+blp = Blueprint('items', __name__, description='Operations on items')
+
+@blp.route('/item')
+class ItemList(MethodView):
+    @jwt_required()
+    @blp.response(200, ItemSchema(many=True))
+    def get(self):
+        items = ItemModel.query.all()
+        return items
+
+    @jwt_required(fresh=True)
+    @blp.arguments(ItemSchema)  # item_data is in ItemSchema, ItemSchema collects JSON
+    @blp.response(201, ItemSchema)
+    def post(self, item_data):
+        item = ItemModel(**item_data)
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message='An error occured while inserting item')
+        return item
+
+@blp.route('/item/<int:item_id>')
+class Item(MethodView):
+    @jwt_required()
+    @blp.response(200, ItemSchema)
+    def get(self, item_id):
+        item = ItemModel.query.get_or_404(item_id)
+        return item
+    
+    @jwt_required()
+    def delete(self, item_id):
+        jwt = get_jwt()
+        if not jwt.get('is_admin'):
+            abort(401, message='Admin privilege required!')
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return {'message':'Item is deleted'}
+
+    
+    @blp.arguments(ItemUpdateSchema)  #item_data from SCHEMA must be just after SELF
+    @blp.response(200, ItemSchema)
+    def put(self,  item_data, item_id):
+        item = ItemModel.query.get(item_id)
+        if item:
+            item.price = item_data['price']
+            item.name = item_data['name']
+        else:
+            item = ItemModel(id=item_id, **item_data)
+
+        db.session.add(item)
+        db.session.commit()
+
+        return item
